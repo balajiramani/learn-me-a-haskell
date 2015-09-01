3e130890901c0c5197544a11c0996cf7b2aeaa44
R <balajir@amazon.com>
2015-09-01 00:02:34 -0700

    Adding supposed quicksort to recursion.hs

--- recursion.hs
+++ recursion.hs
@@ -2,3 +2,10 @@ mymaximum :: (Ord a) => [a] -> a
 mymaximum [] = error "mymaximum of empty list"
 mymaximum [x] = x
 mymaximum (x:xs) =  max x (mymaximum xs)
+
+wrongQuickSort :: (Ord a) => [a] -> [a]
+wrongQuickSort [] = []
+wrongQuickSort (x:xs) = 
+			let lower = wrongQuickSort [a | a <- xs, a <= x]
+			    higher = wrongQuickSort [a | a <- xs, a > x]
+                            in lower ++ [x] ++ higher
